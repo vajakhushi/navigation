e141a8da20ae8a50b3756394a1f493a68aa9ec09
vajakhushi <132640016+vajakhushi@users.noreply.github.com>
2023-12-05 08:23:55 +0530

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -27,26 +27,10 @@ function Home() {
 
     },
   ]
-
  
   return (
-
-   
-
    
     <div>
-      {/* <div className='home'>
-        <div class='row'>
-          <div class="col-md-6 text-center">
-          <h2>WE DESIGNING YOUR SPACE</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic maxime velit nihil ipsum adipisci! Et magni perferendis, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium dicta quis minima odit? Aperiam, voluptates. Earum, explicabo incidunt maxime corporis dolor hic non eveniet vero aut magnam suscipit nulla similique?</p>
-        <button class="btn btn-outline-warning">Learn More</button>
-          </div>
-          <div class="col-md-6">
-            <img src="https://devvlsnxxkrq9.cloudfront.net/prod/assets/_950xAUTO_crop_center-center_none/land_2.jpg" class="d-block w-300"/>
-          </div>
-        </div>
-      </div> */}
     <div class="row col-md-12">
     <div class="card" style={{width: "18rem"}}>
   <img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509_640.jpg" alt="Card image cap"/>
@@ -56,7 +40,6 @@ function Home() {
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-
 
 </div>
     </div>
